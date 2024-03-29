baa1e481bc6469c0f25daec3b92e6c17a1271017
antrinarich <antrinarich@gmail.com>
2024-03-29 15:18:38 +0100

    Apply Betty style fixes to improve code readability and consistency

--- _printinteger.c
+++ _printinteger.c
@@ -1,39 +1,43 @@
 #include "main.h"
 /**
- * _printinteger - print a number (integer) to the standard output.
- * @args: a va_list containing the integer to be printed.
- * Return: the number of characters printed.
- */
+* _printinteger - print a number (integer) to the standard output.
+* @args: a va_list containing the integer to be printed.
+* Return: the number of characters printed.
+*/
 int _printinteger(va_list args)
 {
-    long int number = va_arg(args, int);
-    long int absolute_number = 0;
-    long int temp_number = absolute_number;
-    long int digit_position = 1;
-    int lenght = 0;
+	long int number = va_arg(args, int);
 
-    if (number < 0)
-    {
-        absolute_number = (number * -1);
-        _putchar('-');
-        lenght++;
-    }
-    else
-    {
-        absolute_number = number;
-    }
-    temp_number = absolute_number;
-    while (temp_number > 9)
-    {
-        temp_number = temp_number / 10;
-        digit_position = digit_position * 10;
-    }
-    while (digit_position >= 1)
-    {
-        _putchar(((absolute_number / digit_position) % 10) + '0');
-        digit_position = digit_position / 10;
+	long int absolute_number = 0;
 
-        lenght++;
-    }
-    return (lenght);
+	long int temp_number = absolute_number;
+
+	long int digit_position = 1;
+
+	int lenght = 0;
+
+	if (number < 0)
+	{
+		absolute_number = (number * -1);
+		_putchar('-');
+		lenght++;
+	}
+	else
+	{
+		absolute_number = number;
+	}
+	temp_number = absolute_number;
+	while (temp_number > 9)
+	{
+		temp_number = temp_number / 10;
+		digit_position = digit_position * 10;
+	}
+	while (digit_position >= 1)
+	{
+		_putchar(((absolute_number / digit_position) % 10) + '0');
+		digit_position = digit_position / 10;
+
+		lenght++;
+	}
+	return (lenght);
 }
