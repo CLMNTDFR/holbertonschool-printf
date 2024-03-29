ec7fdea05403544ec6b63b0573a116b14623af4d
antrinarich <antrinarich@gmail.com>
2024-03-29 15:27:29 +0100

    fix betty style and documentation in file _printstring.c

--- _printstring.c
+++ _printstring.c
@@ -3,17 +3,18 @@
 /**
 * _printstring - prints a string
 * @args: list of arguments from variadic function printf
-
+*
 * Return: int (length of the printed string)
 */
 int _printstring(va_list args)
 {
-    char *string = va_arg(args, char *);
-    int index;
+	char *string = va_arg(args, char *);
 
-    for (index = 0; string[index] != '\0'; index++)
-    {
-        _putchar(string[index]);
-    }
-    return (index);
+	int index;
+
+	for (index = 0; string[index] != '\0'; index++)
+	{
+		_putchar(string[index]);
+	}
+	return (index);
 }
